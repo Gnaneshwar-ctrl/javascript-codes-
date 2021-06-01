63dfc1feea44986a65c79dee56435375a3669a6c
Gnaneshwar <63533926+Gnaneshwar-ctrl@users.noreply.github.com>
2021-06-01 18:39:29 +0530

    Add files via upload

---
+++ event listner/script.js
@@ -0,0 +1,15 @@
+const HOD = document.querySelector(".HOD");
+const faculty = document.querySelector(".faculty");
+const student = document.querySelector(".student");
+
+HOD.addEventListener("click", e => {
+    alert("this is HOD")
+})
+
+faculty.addEventListener("click", e => {
+    alert("this is faculty")
+})
+
+student.addEventListener("click", e => {
+    alert("this is student")
+})
